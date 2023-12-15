26631fa7c8bfd1cc73a2423139818e504faac0dc
irfan <23pointalpha@gmail.com>
2023-12-15 11:18:05 +0300

    part8-swagger implementation completed. http://localhost:8762/webjars/swagger-ui/index.html

--- user-service/src/main/java/com/cydeo/UserServiceApplication.java
+++ user-service/src/main/java/com/cydeo/UserServiceApplication.java
@@ -1,5 +1,8 @@
 package com.cydeo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +12,13 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(servers = {
+        @Server(url = "/user-service")
+}, info =
+@Info(title = "Ticketing App", version = "1.0", description = "User Service API"))
 public class UserServiceApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(UserServiceApplication.class,args);
     }
 
